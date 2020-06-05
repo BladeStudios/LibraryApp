51e235980d5d5cf995e9f1cc973485ac64730160
Blade Studios <bladestudiosit@gmail.com>
2020-06-05 19:42:55 +0200

    - dodano menu - dodano trochę styli css w menu - dodano komponenty login, bookslist, userpanel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BookslistComponent } from './bookslist/bookslist.component';
+import { UserpanelComponent } from './userpanel/userpanel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BookslistComponent,
+    UserpanelComponent
   ],
   imports: [
     BrowserModule
